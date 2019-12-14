9dafa94c201ce937b15ae6b899181ccef21b3481
TommyGenovese <tommaso.genovese@itiscuneo.eu>
2019-12-14 13:07:53 +0100

    add

--- Python/es4/es4.py
+++ Python/es4/es4.py
@@ -1,6 +1,6 @@
 
-import random
+import random as a_caso
 
 nameList=["Mario Rossi", "Luca Verdi", "Maria Vergine"]
 
-print(f"viene interrogatoil numero: {random.randint(0,len(nameList))}")
+print(f"viene interrogatoil numero: {a_caso.randint(0,len(nameList))}")
--- Python/es5/es5.py
+++ Python/es5/es5.py
@@ -1,7 +1,12 @@
 
-import random
-
 nameList=["Mario Rossi", "Luca Verdi", "Maria Vergine"]
 
 for num, student in enumerate(nameList):
     print(f"{num} - {student}")
+
+#scambio:
+a =10
+b = 5
+print(f"a:{a}, b: {b}")
+a, b = b, a
+print(f"a:{a}, b: {b}")
