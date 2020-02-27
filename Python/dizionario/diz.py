3aaaa9bd490e7372deb2243cb16256785968312f
TommyGenovese <tommaso.genovese@itiscuneo.eu>
2020-02-27 10:34:01 +0100

    adj2dict & dict2adj... working

--- Python/dizionario/diz.py
+++ Python/dizionario/diz.py
@@ -1,28 +1,18 @@
+def adj2dict(matrice):
+    dictionary = {}
+    for n in range(len(matrice)):
+        dictionary[n]=matrice[n]
+    return dictionary
 
-grafo = {
-    "n1" : 35,
-    "n2" : 4,
-    "n3" : 15,
-    "n4" : 25,
-    "n5" : 134,
-}
-print(grafo)
+def dict2adj(diz):
+    matrix= []
+    for _, i in diz.items():
+        matrix.append(i)
+    return matrix
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#main
+mat= [[2],[1,3]]
+diz=adj2dict(mat)
+print(f"\nStampa del dizionario ottenuto da adj2dict:\n{diz}\n")
+mat = dict2adj(diz)
+print(f"ora stampiamo la matrice ottenuta da dict2adj: \n{mat}\n")
