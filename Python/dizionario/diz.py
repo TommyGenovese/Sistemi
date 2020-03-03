0ce58dc1b4f483103d4c382932c345b1520f5023
TommyGenovese <tommaso.genovese@itiscuneo.eu>
2020-03-03 10:56:29 +0100

    diz

--- Python/dizionario/diz.py
+++ Python/dizionario/diz.py
@@ -10,9 +10,24 @@ def dict2adj(diz):
         matrix.append(i)
     return matrix
 
+def user2dictPes():
+    nodi = int(input("Inserire numero di nodi: "))
+    matrix = []
+    for i in range(0,nodi):
+        neighbor = [int(n) for n in input(f"Inserisci i pesi dei collegamneti fra i nodi in ordine {i+1}: ").split(",")]
+        colonne = [0 for n in range(0,nodi)]
+        for n in neighbor:
+            if(n != -1):
+                colonne[n] = 1
+        matrix.append(colonne)
+    return matrix
+
+
 #main
 mat= [[2],[1,3]]
 diz=adj2dict(mat)
-print(f"\nStampa del dizionario ottenuto da adj2dict:\n{diz}\n")
+#print(f"\nfStampa del dizionario ottenuto da adj2dict:\n{diz}\n")
 mat = dict2adj(diz)
-print(f"ora stampiamo la matrice ottenuta da dict2adj: \n{mat}\n")
+#print(f"ora stampiamo la matrice ottenuta da dict2adj: \n{mat}\n")
+
+print(user2dictPes())
