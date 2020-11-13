531b2d37b3b4a9dd534f87c6968771c156c70b92
TommyGenovese <48619436+TommyGenovese@users.noreply.github.com>
2020-11-13 12:55:17 +0100

    eg

--- Sistemi_5°/Esercizi/FattoriPrimi.py
+++ Sistemi_5°/Esercizi/FattoriPrimi.py
@@ -1,7 +1,7 @@
 #Author: Genovese Tommaso-
 #Date: 11/11/2020
 #es: Dato un numero, scomporlo in fattori primi.
-import math
+import math, time
 def num_primo(n):
     lista_num = []
     for k in range (2,int(math.sqrt(n) + 1)): #
@@ -14,20 +14,22 @@ def num_primo(n):
 
 #main
 
-n = int(input("inserisci il numero da scomporre: "))
+n = int(input("insert the number: "))
 if num_primo(n):
     print("Your number is already a prime number")
     print(n)
 else:
-    ListaFatt = []    
+    ListFact = []
     current = n
     for i in range(2,int(n/2)+1):
         #j = 1
         j=1
         while j==1:
             if (current % i) == 0:
-                ListaFatt.append(i)
+                ListFact.append(i)
                 current = current / i
             else:
                 j = 0
-    print(f"il numero è scomponibile in questi fattori: {ListaFatt}")
+    print(f"The number has been broken down: {ListaFatt}")
+
+time.sleep(500)
--- Sistemi_5°/Esercizi/euclide.py
+++ Sistemi_5°/Esercizi/euclide.py
@@ -1,16 +1,19 @@
 #Author: Genovese Tommaso
 #Date:11/11/2020
-#es:Implementare Euclide (cazz'è?): In ogni triangolo rettangolo il 
+#es:Implementare l'algoritmo Euclide (cazz'è?): In ogni triangolo rettangolo il 
 #   quadrato costruito su un cateto è equivalente al rettangolo che ha per
 #   dimensioni l'ipotenusa e la proiezione di quel cateto sull'ipotenusa.
-
-a = int(input("Inserisci il valore di A(più grande del successivo): "))
-b = int(input("Inserisci il valore di B(più piccolo del precedente): "))
+import time
+a = int(input("Insert A value (bigger than the next ): "))
+b = int(input("Insert B value (smaller than the previous one): "))
 while(1):
-    up_a = a % b    
+    up_a = a % b   
+
     if up_a == 0:   
-        print(f"il valore di b è: {b}")
+        print(f"The greatest common divisor is: {b}")
         break
     else:
-        a=b
         b=up_a
+        a=b
+        
+time.sleep(3)
